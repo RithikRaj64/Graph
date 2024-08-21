50f2339472be6d48bba7be3da87ab180675a8b1e
Rithik Raj <Rithik.Raj.K.S@ibm.com>
2024-08-21 10:26:34 +0530

    Initial Commit

---
+++ graph/exceptions.py
@@ -0,0 +1,22 @@
+class StorageContextNotFoundException(Exception):
+    pass
+
+
+class LLMNotFoundException(Exception):
+    pass
+
+
+class EmbeddingModelNotFoundException(Exception):
+    pass
+
+
+class InvlaidModelIdException(Exception):
+    pass
+
+
+class KnowledgeBaseAlreadyExists(Exception):
+    pass
+
+
+class KnowledgeBaseNotFound(Exception):
+    pass
